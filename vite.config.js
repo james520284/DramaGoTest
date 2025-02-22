6b0556994a2b5ed1d18557889c1423f7343f99a0
james520284 <james520284@gmail.com>
2025-02-22 19:40:10 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   // base 的寫法：
-  base: '/DramaGo/',
+  base: '/DramaGoTest/',
 
   plugins: [react()],
 });
